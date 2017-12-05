08b36549c81a1101d556562e15fb7e5d83b3e879
Andrew Kirkegaard <andrew@farmlogs.com>
2017-12-05 12:11:43 -0600

    Add day 5 solutions

---
+++ day5/src/part1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<int> jumps { istream_iterator<int>(cin), istream_iterator<int>() };
+
+  const int size = jumps.size();
+  int i = 0;
+  int step = 0;
+
+  while (i >= 0 && i < size) {
+    int& jump = jumps.at(i);
+    i += jump;
+    jump++;
+    step++;
+  }
+
+  cout << step << endl;
+
+  return EXIT_SUCCESS;
+}
---
+++ day5/src/part2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<int> jumps { istream_iterator<int>(cin), istream_iterator<int>() };
+
+  const int size = jumps.size();
+  int i = 0;
+  int step = 0;
+
+  while (i >= 0 && i < size) {
+    int& jump = jumps.at(i);
+    i += jump;
+
+    if (jump >= 3) {
+      jump--;
+    } else {
+      jump++;
+    }
+
+    step++;
+  }
+
+  cout << step << endl;
+
+  return EXIT_SUCCESS;
+}
